10c12b0947d5e0e71c4b6e001275b1634cbce5ba
Daniele D'Agostino <principelele@gmail.com>
2020-06-24 15:15:27 +0200

    first commit

---
+++ src/test/java/uk/co/wowcher/test/ClockServiceTest.java
@@ -0,0 +1,81 @@
+package uk.co.wowcher.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.co.wowcher.utility.ClockService;
+
+public class ClockServiceTest {
+
+	private ClockService service;
+
+	@Before
+	public void setUp() {
+		service = new ClockService();
+	}
+
+	@Test
+	public void A_test() {
+		String time = "10:00";
+
+		String result = service.convertToWords(time);
+
+		assertEquals("It's ten", result);
+	}
+
+	@Test
+	public void B_test() {
+		String time = "12:00";
+
+		String result = service.convertToWords(time);
+
+		assertEquals("It's midday", result);
+	}
+
+	@Test
+	public void C_test() {
+		String time = "00:00";
+
+		String result = service.convertToWords(time);
+
+		assertEquals("It's midnight", result);
+	}
+
+	@Test
+	public void D_test() {
+		String time = "11:12";
+
+		String result = service.convertToWords(time);
+
+		assertEquals("It's eleven twelve", result);
+	}
+
+	@Test
+	public void E_test() {
+		String time = "23:34";
+
+		String result = service.convertToWords(time);
+
+		assertEquals("It's twenty three thirty four", result);
+	}
+	
+	@Test
+	public void F_test() {
+		String time = "00:34";
+
+		String result = service.convertToWords(time);
+
+		assertEquals("It's midnight thirty four", result);
+	}
+	
+	@Test
+	public void G_test() {
+		String time = "19:30";
+
+		String result = service.convertToWords(time);
+
+		assertEquals("It's nineteen thirty", result);
+	}
+}
